53b971902ba55673139b3cef37986a69a5b6bdf3
Guilherme <guinantes27@gmail.com>
2024-07-22 23:17:45 -0400

    mais exerciso DIa de baiano em busca da meia hora

---
+++ Programas de Python/ex8Funçao.py
@@ -0,0 +1,6 @@
+def tep(celso:float):
+    f = celso * (9.0/5.0) + 32.0
+    print(f" Tempetura {celso}C° convertido {f}F° ")
+
+tep(45)
+tep(72)
---
+++ Programas de Python/ex9Funçao.py
@@ -0,0 +1,5 @@
+def volumec(raio,altura):
+    V = (raio**2) * altura * 3.141592
+    print(V)
+
+volumec(34,60)
