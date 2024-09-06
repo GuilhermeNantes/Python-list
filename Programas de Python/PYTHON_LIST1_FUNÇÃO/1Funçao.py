a6bc7f62d8fc12faa97118871e8eb1515361e929
Guilherme <guinantes27@gmail.com>
2024-09-06 18:48:21 -0400

    atulizaçao13

--- Programas de Python/PYTHON_LIST1_FUNÇÃO/1Funçao.py
+++ Programas de Python/PYTHON_LIST1_FUNÇÃO/1Funçao.py
@@ -1,6 +1,8 @@
-def pot(a,b):
-    for i in range(1,b+1,1):
-        soma = a ** i
-        print(f"{a} ** {i} = {soma}")
+# def pot(a,b):
+#     for i in range(1,b+1,1):
+#         soma = a ** i
+#         print(f"{a} ** {i} = {soma}")
 
-pot(2,3) 
+# pot(2,3)
+x = ((2**2)+(6**2))* .5
+print(x)
